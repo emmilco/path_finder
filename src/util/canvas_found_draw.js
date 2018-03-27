b58bb6c4bc17ed2f787314d894672dc7581c0725
EMMilco <elliot.milco@gmail.com>
2018-03-26 23:41:29 -0400

    Remove vestigial code from draw util files

--- src/util/canvas_found_draw.js
+++ src/util/canvas_found_draw.js
@@ -5,17 +5,4 @@ function canvasFoundDraw(node, ctx){
   ctx.fillRect(node.x * size,node.y * size, size, size);
 }
 
-
-// function canvasFoundDraw(node, ctx){
-//   const size = 20;
-//   ctx.rect(node.x * size,node.y * size, size, size);
-//   ctx.stroke();
-//   ctx.font="9px Georgia";
-//   ctx.textAlign="center";
-//   ctx.textBaseline = "middle";
-//   ctx.fillStyle = "#000000";
-//   const distance = node.distance();
-//   ctx.fillText(distance, size*node.x+(size/2), size*node.y+(size/2));
-// }
-
 export default canvasFoundDraw;
